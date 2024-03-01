0751dd2f42801c2b85359b6680a23cfbc3faa7bc
Harshit <harshit0580.be21@chitkara.edu.in>
2024-03-01 22:20:43 +0530

    changePassword controller (raw)

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -223,3 +223,78 @@ exports.login = async (req, res) => {
         })
     }
 }
+
+
+// change password
+exports.changePassword = async (req, res) => {
+    try {
+        const token = req.cookies.token || req.body.token || req.header('Authorisation').replace('Bearer ', "");
+        // get data from body
+        // get oldPassword, newPassword, confirmPassword
+        const {oldPassword, newPassword, confirmPassword} = req.body;
+        // validation
+        if(!oldPassword || !newPassword || !confirmPassword){
+            return res.status(403).json({
+                success: false,
+                message: 'All fields are required'
+            })
+        }
+
+        if(newPassword !== confirmPassword) {
+            return res.status(400).json({
+                success: true,
+                message: 'Password do not match'
+            })
+        }
+
+        // find user by id
+        try {
+            const decode = await jwt.verify(token, process.env.JWT_SECRET);
+            console.log(decode);
+            req.user = decode;
+        } catch (error) {
+            // verification issue
+            return res.status(401).json({
+                success: false,
+                message: 'Token invalid'
+            })
+        }
+        const userId = req.user._id;
+        const user = await User.findById(userId);
+
+        if(!user) {
+            return res.status(404).json({
+                success: true,
+                message: 'User not found'
+            })
+        }
+
+        const isPasswordValid = await bcrypt.compare(oldPassword, user.password);
+        if(!isPasswordValid) {
+            return res.status(401).json({
+                success: true,
+                message: 'Incorrect old password'
+            })
+        }
+
+        // update password in Db
+        const hashedPassword = bcrypt.hash(newPassword, 10);
+
+        user.password = hashedPassword;
+        await user.save();
+
+        // send mail - Password updated
+
+        // return response
+        return res.status(200).json({
+            success: true,
+            message: 'Password changed successfully'
+        })
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            success: false,
+            message: 'Error changing password'
+        })
+    }
+}
