f9d47a14d758591b39e6b08ab2f1f1a88ae3b37e
Harshit <harshit0580.be21@chitkara.edu.in>
2024-03-01 20:26:08 +0530

    Login Controller

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,8 +1,10 @@
 const User = require('../models/User')
 const OTP = require('../models/OTP')
 const Profile = require('../models/Profile')
+require('dotenv').config()
 const otpGenerator = require('otp-generator')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 
 exports.sendOtp = async (req, res) => {
@@ -155,6 +157,69 @@ exports.signUp = async (req, res) => {
         return res.status(500).json({
             success: false,
             message: "User can't be regitered, please try again"
+        })
+    }
+}
+
+
+// Login
+exports.login = async (req, res) => {
+    try {
+        // get data from body
+        const {email, password} = req.body;
+
+        // validation data
+        if(!email || !password){
+            return res.status(403).json({
+                success: false,
+                message: 'All fields are required'
+            })
+        }
+
+        // check if user exists 
+        const user = await User.findOne({email}).populate("additionalDetails");
+        if(!user) {
+            return res.status(401).json({
+                success: false,
+                message: 'User not registered'
+            })
+        }
+
+        // validate password
+        if(await bcrypt.compare(password, user.password)) {
+            const payload = {
+                email: user.email,
+                id: user._id,
+                role: user.role
+            }
+            const token = jwt.sign(payload, process.env.JWT_SECRET, {
+                expiresIn: '2h'
+            })
+            user.token = token;
+            user.password = undefined;
+
+            // create cookie and send response
+            const options = {
+                expires: new Date(Date.now() + 3*24*60*60*1000),
+                httpOnly: true,
+            }
+            res.cookie('token', token, options).status(200).json({
+                success: true,
+                token,
+                user,
+                message: 'Logged In'
+            })
+        } else {
+            return res.status(401).json({
+                success: false,
+                message: 'Password Incorrect'
+            })
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            success: false,
+            message: 'Login failure please try again'
         })
     }
 }
